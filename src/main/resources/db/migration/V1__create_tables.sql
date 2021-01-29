42d8c75606e3384599a4e7ed0529896327b6e0dc
Vitor Villar <vitor-luis.villar-neto@sixt.com>
2021-01-29 12:11:33 +0100

    Added uuid extension

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,15 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 create table collection_customer
 (
-    id        uuid         not null,
+    id        uuid         not null default uuid_generate_v4(),
     addresses jsonb        not null,
     email     varchar(255) not null,
     name      varchar(255) not null,
     primary key (id)
 );
 
+create table reservation
+(
+    id             uuid  not null default uuid_generate_v4(),
+    charge_package jsonb not null,
+    primary key (id)
+);
+
 create table simple_customer
 (
-    id               uuid         not null,
+    id               uuid         not null default uuid_generate_v4(),
     billing_address  jsonb        not null,
     delivery_address jsonb        not null,
     email            varchar(255) not null,
